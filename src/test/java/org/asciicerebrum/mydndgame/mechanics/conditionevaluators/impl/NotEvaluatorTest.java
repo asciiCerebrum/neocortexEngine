f16838f83a9b052d76bc87c74542fd1b3b3f425e
asciiCerebrum <asciicerebrum@gmail.com>
2015-02-23 22:52:25 +0100

    more tests

--- src/test/java/org/asciicerebrum/mydndgame/mechanics/conditionevaluators/impl/NotEvaluatorTest.java
+++ src/test/java/org/asciicerebrum/mydndgame/mechanics/conditionevaluators/impl/NotEvaluatorTest.java
@@ -1,17 +1,29 @@
 package org.asciicerebrum.mydndgame.mechanics.conditionevaluators.impl;
 
+import org.asciicerebrum.mydndgame.domain.core.ICharacter;
+import org.asciicerebrum.mydndgame.domain.core.UniqueEntity;
+import org.asciicerebrum.mydndgame.domain.game.DndCharacter;
+import org.asciicerebrum.mydndgame.domain.game.Weapon;
+import org.asciicerebrum.mydndgame.mechanics.conditionevaluators.ConditionEvaluator;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
  * @author species8472
  */
 public class NotEvaluatorTest {
+
+    private NotEvaluator evaluator;
+
+    private ConditionEvaluator conditionEvaluator;
 
     public NotEvaluatorTest() {
     }
@@ -26,24 +38,35 @@ public class NotEvaluatorTest {
 
     @Before
     public void setUp() {
+        this.evaluator = new NotEvaluator();
+        this.conditionEvaluator = mock(ConditionEvaluator.class);
 
+        this.evaluator.setConditionEvaluator(this.conditionEvaluator);
     }
 
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of evaluate method, of class NotEvaluator.
-     */
     @Test
-    public void testEvaluate() {
-        fail();
+    public void evaluateFalseTest() {
+        final ICharacter dndCharacter = new DndCharacter();
+        final UniqueEntity contextItem = new Weapon();
+        when(this.conditionEvaluator.evaluate(dndCharacter, contextItem))
+                .thenReturn(Boolean.TRUE);
+        final boolean result = this.evaluator.evaluate(
+                dndCharacter, contextItem);
+        assertFalse(result);
     }
 
     @Test
-    public void testEvaluateOpposite() {
-        fail();
+    public void evaluateTrueTest() {
+        final ICharacter dndCharacter = new DndCharacter();
+        final UniqueEntity contextItem = new Weapon();
+        when(this.conditionEvaluator.evaluate(dndCharacter, contextItem))
+                .thenReturn(Boolean.FALSE);
+        final boolean result = this.evaluator.evaluate(
+                dndCharacter, contextItem);
+        assertTrue(result);
     }
-
 }
