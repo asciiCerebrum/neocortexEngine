1c0b3db9acb93542e31e69b17119dec947116e3f
asciiCerebrum <asciicerebrum@gmail.com>
2015-02-27 20:59:24 +0100

    bugfix, refactoring, tests

--- src/main/java/org/asciicerebrum/mydndgame/managers/CombatRoundManager.java
+++ src/main/java/org/asciicerebrum/mydndgame/managers/CombatRoundManager.java
@@ -1,27 +1,15 @@
 package org.asciicerebrum.mydndgame.managers;
 
-import java.util.Iterator;
 import javax.naming.OperationNotSupportedException;
 import org.asciicerebrum.mydndgame.domain.game.Campaign;
 import org.asciicerebrum.mydndgame.domain.game.DndCharacters;
 import org.asciicerebrum.mydndgame.domain.mechanics.workflow.Interaction;
-import org.asciicerebrum.mydndgame.domain.mechanics.workflow.IWorkflow;
 
 /**
  *
  * @author species8472
  */
-public class CombatRoundManager {
-
-    /**
-     * Workflow for initialization of a combat encounter.
-     */
-    private IWorkflow initializeCombatRoundWorkflow;
-
-    /**
-     * Workflow for the expiry of conditions.
-     */
-    private IWorkflow conditionExpirationWorkflow;
+public interface CombatRoundManager {
 
     /**
      * Initiates the combat round. Only one round can be initiated at the same
@@ -29,35 +17,11 @@ public class CombatRoundManager {
      *
      * @param campaign contains the combat round.
      * @param participants The participants of the combat round.
-     * @return true if combat round could be initiated, false otherwise.
      * @throws javax.naming.OperationNotSupportedException could be thrown due
      * to the call of other workflows.
      */
-    public final Boolean initiateCombatRound(final Campaign campaign,
-            final DndCharacters participants)
-            throws OperationNotSupportedException {
-
-        Interaction interaction = new Interaction();
-        interaction.setTargetCharacters(participants);
-
-        if (this.initializeCombatRoundWorkflow == null) {
-            // in this case it does not make sense to continue.
-            return Boolean.TRUE;
-        }
-
-        this.initializeCombatRoundWorkflow.runWorkflow(interaction);
-
-        campaign.setCombatRound(interaction.getCombatRound());
-
-        // this workflow can also be not setup!
-        if (this.conditionExpirationWorkflow != null) {
-            // first participant in row must get rid of the flat footed
-            // condition.
-            this.conditionExpirationWorkflow.runWorkflow(interaction);
-        }
-
-        return Boolean.TRUE;
-    }
+    void initiateCombatRound(Campaign campaign, DndCharacters participants)
+            throws OperationNotSupportedException;
 
     /**
      * Retrieves the desired interaction from the character and executes it.
@@ -66,38 +30,5 @@ public class CombatRoundManager {
      * @param campaign the campaign this interaction takes place in.
      * @param interaction the interaction in question.
      */
-    public final void executeInteraction(final Campaign campaign,
-            final Interaction interaction) {
-
-        // reject when it's not the characters turn.
-        if (!campaign.getCombatRound().isCurrentParticipant(
-                interaction.getTriggeringCharacter())) {
-            return;
-        }
-
-        final Iterator<IWorkflow> wfIterator
-                = interaction.getInteractionType().getWorkflows().iterator();
-        while (wfIterator.hasNext()) {
-            final IWorkflow workflow = wfIterator.next();
-            workflow.runWorkflow(interaction);
-        }
-    }
-
-    /**
-     * @param initializeCombatRoundWorkflowInput the
-     * initializeCombatRoundWorkflow to set
-     */
-    public final void setInitializeCombatRoundWorkflow(
-            final IWorkflow initializeCombatRoundWorkflowInput) {
-        this.initializeCombatRoundWorkflow = initializeCombatRoundWorkflowInput;
-    }
-
-    /**
-     * @param conditionExpirationWorkflowInput the conditionExpirationWorkflow
-     * to set
-     */
-    public final void setConditionExpirationWorkflow(
-            final IWorkflow conditionExpirationWorkflowInput) {
-        this.conditionExpirationWorkflow = conditionExpirationWorkflowInput;
-    }
+    void executeInteraction(Campaign campaign, Interaction interaction);
 }
---
+++ src/main/java/org/asciicerebrum/mydndgame/managers/DefaultCombatRoundManager.java
@@ -0,0 +1,82 @@
+package org.asciicerebrum.mydndgame.managers;
+
+import java.util.Iterator;
+import javax.naming.OperationNotSupportedException;
+import org.asciicerebrum.mydndgame.domain.game.Campaign;
+import org.asciicerebrum.mydndgame.domain.game.DndCharacters;
+import org.asciicerebrum.mydndgame.domain.mechanics.workflow.IWorkflow;
+import org.asciicerebrum.mydndgame.domain.mechanics.workflow.Interaction;
+
+/**
+ *
+ * @author species8472
+ */
+public class DefaultCombatRoundManager implements CombatRoundManager {
+
+    /**
+     * Workflow for initialization of a combat encounter.
+     */
+    private IWorkflow initializeCombatRoundWorkflow;
+
+    /**
+     * Workflow for the expiry of conditions.
+     */
+    private IWorkflow conditionExpirationWorkflow;
+
+    @Override
+    public final void initiateCombatRound(final Campaign campaign,
+            final DndCharacters participants)
+            throws OperationNotSupportedException {
+
+        Interaction interaction = new Interaction();
+        interaction.setTargetCharacters(participants);
+
+        if (this.initializeCombatRoundWorkflow != null) {
+            this.initializeCombatRoundWorkflow.runWorkflow(interaction);
+            campaign.setCombatRound(interaction.getCombatRound());
+        }
+
+        // this workflow can also be not setup!
+        if (this.conditionExpirationWorkflow != null) {
+            // first participant in row must get rid of the flat footed
+            // condition.
+            this.conditionExpirationWorkflow.runWorkflow(interaction);
+        }
+    }
+
+    @Override
+    public final void executeInteraction(final Campaign campaign,
+            final Interaction interaction) {
+
+        // reject when it's not the characters turn.
+        if (!campaign.getCombatRound().isCurrentParticipant(
+                interaction.getTriggeringCharacter())) {
+            return;
+        }
+
+        final Iterator<IWorkflow> wfIterator
+                = interaction.getInteractionType().getWorkflows().iterator();
+        while (wfIterator.hasNext()) {
+            final IWorkflow workflow = wfIterator.next();
+            workflow.runWorkflow(interaction);
+        }
+    }
+
+    /**
+     * @param initializeCombatRoundWorkflowInput the
+     * initializeCombatRoundWorkflow to set
+     */
+    public final void setInitializeCombatRoundWorkflow(
+            final IWorkflow initializeCombatRoundWorkflowInput) {
+        this.initializeCombatRoundWorkflow = initializeCombatRoundWorkflowInput;
+    }
+
+    /**
+     * @param conditionExpirationWorkflowInput the conditionExpirationWorkflow
+     * to set
+     */
+    public final void setConditionExpirationWorkflow(
+            final IWorkflow conditionExpirationWorkflowInput) {
+        this.conditionExpirationWorkflow = conditionExpirationWorkflowInput;
+    }
+}
